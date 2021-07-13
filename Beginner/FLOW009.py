f2520db2f1495c049ab0dcf9baf19f0811610c75
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2021-07-13 23:45:28 +0530

    Total Expenses

---
+++ Beginner/FLOW009.py
@@ -0,0 +1,8 @@
+# cook your dish here
+try:
+    for _ in range(int(input())):
+        Q, P = map(int, input().split())
+        T = Q*P
+        print(T if Q <= 1000 else T - T*0.1)
+except:
+    pass
