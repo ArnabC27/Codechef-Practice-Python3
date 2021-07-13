5c5b57eb7ff92b47c38713ffe332a3fa72503ecf
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2021-07-13 23:41:32 +0530

    Id and Ship

---
+++ Beginner/FLOW010.py
@@ -0,0 +1,14 @@
+# cook your dish here
+try:
+    for i in range(int(input())):
+        C = input()
+        if C == 'B' or C == 'b':
+            print('BattleShip')
+        elif C == 'C' or C == 'c':
+            print('Cruiser')
+        elif C == 'D' or C == 'd':
+            print('Destroyer')
+        else:
+            print('Frigate')
+except:
+    pass
